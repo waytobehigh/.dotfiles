70b0b07c0d9e8f2453db5df97979ee608ebf2fd6
Michael Vasilkovsky <33276701+waytobehigh@users.noreply.github.com>
2021-06-08 10:16:38 +0100

    Update link.sh

--- link.sh
+++ link.sh
@@ -6,11 +6,12 @@ if [ "$(uname)" == "Darwin" ]; then
 fi
 cd "$(dirname "$(realpath "$0")")"
 
-ln -s $@ "$(pwd)/.zshrc" ~/.zshrc || :
-conda init zsh
+if [ "$(uname)" == "Darwin" ]; then
+	ln -s $@ "$(pwd)/.zshrc" ~/.zshrc || :
+	conda init zsh
+	ln -s $@ "$(pwd)/.bash_profile" ~/.bash_profile || :
+fi
 ln -s $@ "$(pwd)/.tmux.conf" ~/.tmux.conf || :
 ln -s $@ "$(pwd)/.vimrc" ~/.vimrc || :
 ln -s $@ "$(pwd)/.aliases" ~/.aliases || :
-ln -s $@ "$(pwd)/.bash_profile" ~/.bash_profile || :
 ln -s $@ "$(pwd)/.git-prompt.sh" ~/.git-prompt.sh || :
-
